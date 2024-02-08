ecb82719ddbdece6648fb6502bb44ecbf756ed88
YeonSeo Park <138553324+gyeonse0@users.noreply.github.com>
2024-02-08 17:21:06 +0900

    Update SolutionPlotter.py

--- examples/SolutionPlotter.py
+++ examples/SolutionPlotter.py
@@ -16,15 +16,26 @@ class SolutionPlotter:
     """
     def __init__(self, data):
         self.data = data
+        self.drone_colors = ['red', 'blue', 'green', 'orange', 'purple']
+        self.truck_colors = ['cyan', 'magenta', 'yellow', 'lime', 'pink']
+        self.drone_color_index = 0
+        self.truck_color_index = 0
+
+    def get_next_drone_color(self):
+        color = self.drone_colors[self.drone_color_index]
+        self.drone_color_index = (self.drone_color_index + 1) % len(self.drone_colors)
+        return color
+
+    def get_next_truck_color(self):
+        color = self.truck_colors[self.truck_color_index]
+        self.truck_color_index = (self.truck_color_index + 1) % len(self.truck_colors)
+        return color
 
     def plot_current_solution(self, routes, name="Multi_Modal Solution"):
         """
         우리가 뽑아낸 routes 딕셔너리 집합과 solution class를 통해서 현재의 cost와 path를 plot 해주는 함수
         """
         fig, ax = plt.subplots(figsize=(12, 10))
-        cmap = plt.get_cmap('rainbow')
-
-        used_colors = set()  # 사용된 색상을 추적하기 위한 세트
 
         for route_info in routes['route']:
             vtype = route_info['vtype']
@@ -32,30 +43,28 @@ class SolutionPlotter:
             path = route_info['path']
 
             if vtype == 'drone':
-                color = self.generate_unique_color(used_colors)  # 겹치지 않는 색상 생성
-                used_colors.add(tuple(color))
+                color = self.get_next_drone_color()
                 path = path if isinstance(path, list) else path[0]
                 loc_getter = lambda loc: loc[0] if isinstance(loc, tuple) else loc
                 linestyle = '--'
+                offset = 0.0001 * (self.drone_color_index + 1) 
+                linewidth=2
 
             elif vtype == 'truck':
-                color = self.generate_unique_color(used_colors)  # 겹치지 않는 색상 생성
-                used_colors.add(tuple(color))
+                color = self.get_next_truck_color()  # 겹치지 않는 색상 생성
                 path = path if isinstance(path, list) else path[0]
                 loc_getter = lambda loc: loc[0] if isinstance(loc, tuple) else loc
                 linestyle = '-'
-
-            else:
-                color = 'k'
-                loc_getter = lambda loc: loc
+                offset = 0
+                linewidth=1
 
             # 경로 그리기
             ax.plot(
                 [self.data['node_coord'][loc_getter(loc)][0] for loc in path],
-                [self.data['node_coord'][loc_getter(loc)][1] for loc in path],
+                [self.data['node_coord'][loc_getter(loc)][1]+ offset for loc in path],
                 color=color,
                 linestyle=linestyle, 
-                linewidth=2,
+                linewidth=linewidth,
                 marker='.',
                 label=f'{vtype} {vid}'
             )
@@ -64,7 +73,8 @@ class SolutionPlotter:
             for i in range(len(path)-1):
                 start = self.data['node_coord'][loc_getter(path[i])]
                 end = self.data['node_coord'][loc_getter(path[i+1])]
-                ax.annotate("", xy=end, xytext=start, arrowprops=dict(arrowstyle="->", color=color))
+                ax.annotate("", xy=(end[0], end[1] + offset), xytext=(start[0], start[1] + offset), arrowprops=dict(arrowstyle="->", color=color))
+
 
         kwargs = dict(label="Depot", zorder=3, marker="s", s=80)
         ax.scatter(*self.data["node_coord"][self.data["depot"]], c="tab:red", **kwargs)
@@ -75,13 +85,3 @@ class SolutionPlotter:
         ax.set_ylabel("Y-coordinate")
         ax.legend(frameon=False, ncol=3)
         plt.show()
-
-    def generate_unique_color(self, used_colors):
-        """
-        겹치지 않는 색상을 생성하는 함수
-        """
-        while True:
-            color = [random.random() for _ in range(3)]  # 무작위 RGB 값 생성
-            if tuple(color) not in used_colors:
-                return color
-
